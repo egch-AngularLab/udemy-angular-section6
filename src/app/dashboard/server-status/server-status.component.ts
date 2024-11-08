919963812f9143211ec76b758d33bda5df698171
Enrico Giurin <enricogiurin@gmail.com>
2024-11-08 01:18:58 +0100

    using DestroyRef

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,45 +1,48 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+import { interval } from "rxjs";
 
 @Component({
-  selector: 'app-server-status',
+  selector: "app-server-status",
   standalone: true,
   imports: [],
-  templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  templateUrl: "./server-status.component.html",
+  styleUrl: "./server-status.component.css",
 })
 //by implementing this interfaces we are forced to define the specific methods
 //and we well avoid to have wrong behavior in case of typo of the method
-export class ServerStatusComponent implements OnInit, OnDestroy {
+export class ServerStatusComponent implements OnInit {
   //this is kind of enum with a default value ('offline')
-  currentStatus:  'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus: "online" | "offline" | "unknown" = "offline";
 
-  private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
 
   //better to keep your constructor lean
   constructor() {}
 
-
-  //better to use this vs using constructor for initialization 
-  ngOnInit(){
-    setInterval(() => {
+  //better to use this vs using constructor for initialization
+  ngOnInit() {
+    const interval = setInterval(() => {
       const rnd = Math.random(); //0-1(excluded)
-      if(rnd<0.5){
-        this.currentStatus = 'online';
-      }else if (rnd<0.9) {
-        this.currentStatus = 'offline';
-      }else{
-        this.currentStatus = 'unknown';
+      if (rnd < 0.5) {
+        this.currentStatus = "online";
+      } else if (rnd < 0.9) {
+        this.currentStatus = "offline";
+      } else {
+        this.currentStatus = "unknown";
       }
-      
-    }, 5000);  //it will be updated every 5s
+    }, 5000); //it will be updated every 5s
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 
   ngAfterViewInit() {
-    console.log('after view unit');
-  }
-
-  //remove the interval whenever the component is destroy
-  ngOnDestroy() {
-    clearTimeout(this.interval);
+    console.log("after view unit");
   }
 }
