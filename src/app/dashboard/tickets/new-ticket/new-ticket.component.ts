885b2f44eb4d4a5ca0af6b6bf5e021c07598f00f
Enrico Giurin <enricogiurin@gmail.com>
2024-11-12 22:41:14 +0100

    using viewChild() as a signal

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -13,11 +13,17 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent {
 
   //with this I access to the #form from the corresponding html element
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  //@ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+  //now using signal
+  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+
+
+
 
   onSubmit(title: string, ticketText: string) {
     console.dir(title);
     console.dir(ticketText);
-    this.form?.nativeElement.reset();
+    this.form()?.nativeElement.reset();
   }
 }
