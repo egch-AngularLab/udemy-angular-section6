7b936d52929c66ea4280cad0c80ec52b4b278cdf
Enrico Giurin <enricogiurin@gmail.com>
2024-11-13 22:48:08 +0100

    implementing AfterViewInit

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -10,20 +10,31 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
 
-  //with this I access to the #form from the corresponding html element
-  //@ViewChild('form') form?: ElementRef<HTMLFormElement>;
+//I implement the interface in order to avoid typo
+export class NewTicketComponent implements OnInit, AfterViewInit{
+
+    //with this I access to the #form from the corresponding html element
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
 
   //now using signal
-  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
 
+  ngOnInit(): void {
+    console.log('ONINIT');
+    this.form?.nativeElement.reset();
+  }
+
+  ngAfterViewInit(): void {
+    console.log('after view init');
+    console.log(this.form?.nativeElement);
+  }
 
 
   onSubmit(title: string, ticketText: string) {
     console.dir(title);
     console.dir(ticketText);
-    this.form()?.nativeElement.reset();
+    this.form?.nativeElement.reset();
   }
 }
