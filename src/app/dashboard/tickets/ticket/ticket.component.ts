21d9fc5a75f0bcfb9ee82153274baf38ec9997cf
Enrico Giurin <enricogiurin@gmail.com>
2024-11-16 21:07:06 +0100

    Cross-Component communication

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from "@angular/core";
 
 import { NewTicketComponent } from "../new-ticket/new-ticket.component";
+import { Ticket } from "../ticket.model";
 
 @Component({
-  selector: 'app-ticket',
+  selector: "app-ticket",
   standalone: true,
   imports: [NewTicketComponent],
-  templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  templateUrl: "./ticket.component.html",
+  styleUrl: "./ticket.component.css",
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  close = output()
+  detailsVisible = signal(false);
 
- 
+  onToggleDetails() {
+    //this.detailsVisible.set(!this.detailsVisible());
+
+    //an alternative way
+    this.detailsVisible.update((wasVisible) => !wasVisible );
+
+  }
+
+  onMarkAsCompleted() {
+    this.close.emit();
+  }
 }
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,28 +1,36 @@
-import { Component } from '@angular/core';
-import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Component } from "@angular/core";
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { TicketComponent } from "./ticket/ticket.component";
-import { Ticket } from './ticket.model';
+import { Ticket } from "./ticket.model";
 
 @Component({
-  selector: 'app-tickets',
+  selector: "app-tickets",
   standalone: true,
   imports: [NewTicketComponent, TicketComponent],
-  templateUrl: './tickets.component.html',
-  styleUrl: './tickets.component.css'
+  templateUrl: "./tickets.component.html",
+  styleUrl: "./tickets.component.css",
 })
 export class TicketsComponent {
-   //array of Ticket
-   tickets: Ticket[] = [];
+  //array of Ticket
+  tickets: Ticket[] = [];
 
-   onAdd(ticketData: {title: string; text: string}) {
-     const ticket: Ticket = {
-       title: ticketData.title,
-       request: ticketData.text,
-       id: Math.random().toString(),
-       status: 'open'
- 
-     }
-     this.tickets.push(ticket);
-   }
+  onAdd(ticketData: { title: string; text: string }) {
+    const ticket: Ticket = {
+      title: ticketData.title,
+      request: ticketData.text,
+      id: Math.random().toString(),
+      status: "open",
+    };
+    this.tickets.push(ticket);
+  }
 
+  onCloseTicket(id: string): void {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        //this is native JS function
+        return { ...ticket, status: "closed" };
+      }
+      return ticket;
+    });
+  }
 }
